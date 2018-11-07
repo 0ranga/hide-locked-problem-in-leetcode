ee407ed2eca939ac45c15126277015c9de82ea0b
0ranga <zjharpoonman@gmail.com>
2018-11-07 16:04:27 +0800

    add monkey metainfo

--- content_script.js
+++ content_script.js
@@ -1,3 +1,13 @@
+// ==UserScript==
+// @name         Hide Locked Problems in LeetCode
+// @namespace    https://github.com/0ranga/hide-locked-problem-in-leetcode
+// @version      0.1
+// @description  Hide Locked Problems in LeetCode. Originally written by the author @csvwolf and then posted on the monkey by @Nuula.
+// @author       Nuula
+// @match        https://leetcode.com/*
+// @grant        none
+// ==/UserScript==
+
 (function() {
   let target = null;
 
